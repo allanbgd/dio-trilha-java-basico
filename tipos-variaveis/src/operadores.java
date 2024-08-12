e31c5227f39eec255c38498574bc0270b3057f70
Allan Breno <allanbrenogomesdias@gmail.com>
2024-08-12 00:32:57 -0300

    operadores if else

--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -4,8 +4,23 @@ public class operadores {
     
           System.out.println(nomeCompleto);
     
-    
+            String resultado = "";
+
+
+
+            int a, b ;
+            
+            a= 5;
+            b=6;
+            if (a==b)
+            resultado = "verdadeiro";
+            else
+            resultado = "falso";
            
+            //ou 
+            String result = a==b ? "verdadeiro" : "falso";
+
+            System.out.println(result,resultado);
         }
     }
     
