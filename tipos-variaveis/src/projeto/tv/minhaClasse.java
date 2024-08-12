80ab2869cc6c8958655b2f6e235086eed9cf16e3
Allan Breno <allanbrenogomesdias@gmail.com>
2024-08-12 16:59:53 -0300

    continuação nos estudos

---
+++ tipos-variaveis/src/projeto/tv/minhaClasse.java
@@ -0,0 +1,10 @@
+package projeto.tv;
+public class minhaClasse {
+
+    public static void main (String[] argd) throws Exception{
+
+        System.out.println("Oi, fui executado pelo terminal");
+
+    }
+    
+}
