fb9d46483ca4006d6fc7f06d25014ef4c237a936
Allan Breno <allanbrenogomesdias@gmail.com>
2024-08-12 00:04:41 -0300

    aula operadores1

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,12 @@
+public class operadores {
+        public static void main(String[] args) throws Exception {
+          String nomeCompleto = "Linguagem" +"Java";
+    
+          System.out.println(nomeCompleto);
+    
+    
+           
+        }
+    }
+    
+
